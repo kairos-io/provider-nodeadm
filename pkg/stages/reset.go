68f0ebf1941d21ee615f9b32c5d8229afa83b3d5
Tyler Gillson <tyler.gillson@gmail.com>
2024-11-07 13:54:19 -0700

    fix: use bash, not sh
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- pkg/stages/reset.go
+++ pkg/stages/reset.go
@@ -4,5 +4,5 @@ import "os/exec"
 
 // ResetCmd returns the command to reset the nodeadm installation.
 func ResetCmd() *exec.Cmd {
-	return exec.Command("/bin/sh", "-c", resetScript, "--", runtimeRoot)
+	return exec.Command("bash", resetScript, runtimeRoot)
 }
