e4490f46ea7c8dc6dd345d7e0da5667f347aac78
Tyler Gillson <tyler.gillson@gmail.com>
2024-12-12 10:26:28 -0700

    fix: correct args w/ proxy enabled & skip pod validation
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- scripts/nodeadm-upgrade.sh
+++ scripts/nodeadm-upgrade.sh
@@ -63,10 +63,10 @@ run_upgrade() {
     # Upgrade loop, runs until stored and current match
     until [ "$current_version" = "$old_version" ]
     do
-        upgrade_command="nodeadm upgrade -c file://$CONFIG_FILE -d $KUBERNETES_VERSION"
+        upgrade_command="nodeadm upgrade -c file://$CONFIG_FILE -d $KUBERNETES_VERSION --skip pod-validation"
 
         if [ "$PROXY_CONFIGURED" = true ]; then
-          up=("nodeadm upgrade -c $CONFIG_FILE -d $KUBERNETES_VERSION")
+          up=("nodeadm upgrade -c file://$CONFIG_FILE -d $KUBERNETES_VERSION --skip pod-validation")
           upgrade_command="${up[*]}"
         fi
 
